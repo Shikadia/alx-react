51501310bb3799ea9ee1b3b3daa2b1578b04188b
Shikadia <otukachinedu661@gmail.com>
2024-06-25 01:48:41 +0100

    Webpack

---
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -0,0 +1,17 @@
+const $ = require('jquery');
+const lod = require('lodash');
+
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append('<p id="count"></p>');
+$('body').append('<p>Copyright - Holberton School</p>');
+
+let count = 0;
+
+function updateCounter() {
+  count++;
+  $('#count').text(`${count} clicks on the button`);
+}
+
+$('button').on('click', lod.debounce(updateCounter, 500));
