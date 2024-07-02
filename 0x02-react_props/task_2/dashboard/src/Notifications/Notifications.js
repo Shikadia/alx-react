0f91170ceeb01b35f1934664cac8ae1d26d7991b
Shikadia <otukachinedu661@gmail.com>
2024-07-03 00:00:08 +0100

    initial commit

---
+++ 0x02-react_props/task_2/dashboard/src/Notifications/Notifications.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./Notifications.css";
+import closeIcon from "../assets/close-icon.png";
+import { getLatestNotification } from "../utils/utils";
+import NotificationItem from "./NotificationItem";
+
+function Notifications() {
+  return (
+    <div className="Notifications">
+      <button
+        aria-label="Close"
+        onClick={console.log("Close button has been clicked")}
+      >
+        <img src={closeIcon} alt="closeIcon" width="10px" />
+      </button>
+      <p>Here is the list of notifications</p>
+      <ul>
+        <NotificationItem type="default" value="New course available" />
+        <NotificationItem type="urgent" value="New resume available" />
+        <NotificationItem type="urgent" html={getLatestNotification()} />
+      </ul>
+    </div>
+  );
+}
+
+export default Notifications;
